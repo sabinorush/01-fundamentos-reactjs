95427f2f33ba661b3aa9e81ba55c68a3ab627a8d
Gustavo Sabino <gu.sabino@hotmail.com>
2025-02-28 17:47:59 -0300

    chore: application sttructure - Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,29 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img 
+            className={styles.avatar} 
+            src="https://pbs.twimg.com/profile_images/1817738431595696128/OOS20G8L_400x400.jpg" 
+          />
+          <div className={styles.authorInfo}>
+            <strong>Gustavo Sabino</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+       <time title="11 de maio ás 8:13h" dateTime="2022-05-11 08:13:00">Publicado há 1hr</time> 
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href="#">jane.design/doctorcare</a></p>
+        <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}

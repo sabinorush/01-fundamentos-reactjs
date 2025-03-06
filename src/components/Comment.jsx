23581ff243ea83c23374e44e7f2522170a7ff224
Gustavo Sabino <gu.sabino@hotmail.com>
2025-03-06 18:07:13 -0300

    chore: react engine - Understanding closures

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -12,7 +12,10 @@ export function Comment({ content, onDeleteComment }) {
   }
 
   function handleLikeComment() {
-    setLikeCount( likeCount + 1 )
+    setLikeCount( (state) => {
+      return state + 1;
+    })
+
   }
 
   return (
